2fb09048f4bd601afe857cf903e9b48961e51512
levent yıldız <lewenty@gmail.com>
2017-07-08 18:05:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+JAVA // Hibernate // Workshop
---
+++ src/main/java/com/company/hibernate/workshop/ws2/Run.java
@@ -0,0 +1,8 @@
+package com.company.hibernate.workshop.ws2;
+
+public class Run {
+
+    public static void main(String[] args) {
+        System.out.println("Run2....");
+    }
+}
